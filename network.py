19aa96e64ab572c44409c1ce5975ba3afae96a43
jeromeyoon <vmax8322@gmail.com>
2016-12-04 22:37:03 +0900

    network modify

--- network.py
+++ network.py
@@ -24,24 +24,31 @@ class networks(object):
         enc7 =tf.nn.relu(g_bn7(conv2d(enc6,self.df_dim*8,k_h=4,k_w=4,d_h=2,d_w=2,name='g_enc7'))) #output:2x2
 	#### Deconder #####
 	g_bn8 = batch_norm(self.batch_size,name='g_bn8')
-	dec1 = tf.nn.dropout(g_bn8(deconv2d(enc7,[self.batch_size,4,4,self.df_dim*8],name='g_dec1')),keep_prob)#output 4x4
+	dec1 = tf.image.resize_nearest_neighbor(enc7,[4,4])
+	dec1 = tf.nn.dropout(g_bn8(conv2d(dec1,self.df_dim*8,k_h=4,k_w=4,name='g_dec1')),keep_prob) #output 4x4
 	dec1 = tf.nn.relu(tf.concat(3,[dec1,enc6]))
 	g_bn9 = batch_norm(self.batch_size,name='g_bn9')
-	dec2 = tf.nn.dropout(g_bn9(deconv2d(dec1,[self.batch_size,8,8,self.df_dim*8],name='g_dec2')),keep_prob)#output 8x8
+	dec2 = tf.image.resize_nearest_neighbor(dec1,[8,8])
+	dec2 = tf.nn.dropout(g_bn9(conv2d(dec1,self.df_dim*8,k_h=4,k_w=4,name='g_dec2')),keep_prob) #output 8x8
 	dec2 = tf.nn.relu(tf.concat(3,[dec2,enc5]))
 	g_bn10 = batch_norm(self.batch_size,name='g_bn10')
-	dec3 = tf.nn.dropout(g_bn10(deconv2d(dec2,[self.batch_size,16,16,self.df_dim*8],name='g_dec3')),keep_prob)#output 16x16
+	dec3 = tf.image.resize_nearest_neighbor(dec2,[16,16])
+	dec3 = tf.nn.dropout(g_bn10(conv2d(dec3,self.df_dim*8,k_h=4,k_w=4,name='g_dec3')),keep_prob) #output 16x16
 	dec3 = tf.nn.relu(tf.concat(3,[dec3,enc4]))
 	g_bn11 = batch_norm(self.batch_size,name='g_bn11')
-	dec4 = g_bn11(deconv2d(dec3,[self.batch_size,32,32,self.df_dim*8],name='g_dec4'))#output 32x32
+	dec4 = tf.image.resize_nearest_neighbor(dec3,[32,32])
+	dec4 = tf.nn.dropout(g_bn11(conv2d(dec4,self.df_dim*8,k_h=4,k_w=4,name='g_dec4')),keep_prob) #output 32x32
 	dec4 = tf.nn.relu(tf.concat(3,[dec4,enc3]))
 	g_bn12 = batch_norm(self.batch_size,name='g_bn12')
-	dec5 = g_bn12(deconv2d(dec4,[self.batch_size,64,64,self.df_dim*4],name='g_dec5'))#output 64x64
+	dec5 = tf.image.resize_nearest_neighbor(dec4,[64,64])
+	dec5 = tf.nn.dropout(g_bn12(conv2d(dec5,self.df_dim*4,k_h=4,k_w=4,name='g_dec5')),keep_prob) #output 64x64
 	dec5 = tf.nn.relu(tf.concat(3,[dec5,enc2]))
 	g_bn13 = batch_norm(self.batch_size,name='g_bn13')
-	dec6 = g_bn13(deconv2d(dec5,[self.batch_size,128,128,self.df_dim*2],name='g_dec6'))#output 128x128
+	dec6 = tf.image.resize_nearest_neighbor(dec5,[128,128])
+	dec6 = tf.nn.dropout(g_bn13(conv2d(dec6,self.df_dim*2,k_h=4,k_w=4,name='g_dec6')),keep_prob) #output 128x128
 	dec6 = tf.nn.relu(tf.concat(3,[dec6,enc1]))
-	dec7 = deconv2d(dec6,[self.batch_size,256,256,3],name='g_dec7')#output 256x256
+	dec7 = tf.image.resize_nearest_neighbor(dec6,[256,256])
+	dec7 = conv2d(dec7,3,k_h=4,k_w=4,name='g_dec7') #output 256x256
 	return tf.nn.tanh(dec7)
 
     def discriminator(self, image, reuse=False):
@@ -80,23 +87,30 @@ class networks(object):
         enc7 =tf.nn.relu(g_bn7(conv2d(enc6,self.df_dim*8,k_h=4,k_w=4,d_h=2,d_w=2,name='g_enc7'),train=False)) #output:2x2
 	#### Deconder #####
 	g_bn8 = batch_norm(self.batch_size,name='g_bn8')
-	dec1 = tf.nn.dropout(g_bn8(deconv2d(enc7,[self.batch_size,4,4,self.df_dim*8],name='g_dec1'),train=False),keep_prob)#output 4x4
+	dec1 = tf.image.resize_nearest_neighbor(enc7,[4,4])
+	dec1 = tf.nn.dropout(g_bn8(conv2d(dec1,self.df_dim*8,k_h=4,k_w=4,name='g_dec1'),train=False),keep_prob) #output 4x4
 	dec1 = tf.nn.relu(tf.concat(3,[dec1,enc6]))
 	g_bn9 = batch_norm(self.batch_size,name='g_bn9')
-	dec2 = tf.nn.dropout(g_bn9(deconv2d(dec1,[self.batch_size,8,8,self.df_dim*8],name='g_dec2'),train=False),keep_prob)#output 8x8
+	dec2 = tf.image.resize_nearest_neighbor(dec1,[8,8])
+	dec2 = tf.nn.dropout(g_bn9(conv2d(dec1,self.df_dim*8,k_h=4,k_w=4,name='g_dec2'),train=False),keep_prob) #output 8x8
 	dec2 = tf.nn.relu(tf.concat(3,[dec2,enc5]))
 	g_bn10 = batch_norm(self.batch_size,name='g_bn10')
-	dec3 = tf.nn.dropout(g_bn10(deconv2d(dec2,[self.batch_size,16,16,self.df_dim*8],name='g_dec3'),train=False),keep_prob)#output 16x16
+	dec3 = tf.image.resize_nearest_neighbor(dec2,[16,16])
+	dec3 = tf.nn.dropout(g_bn10(conv2d(dec3,self.df_dim*8,k_h=4,k_w=4,name='g_dec3'),train=False),keep_prob) #output 16x16
 	dec3 = tf.nn.relu(tf.concat(3,[dec3,enc4]))
 	g_bn11 = batch_norm(self.batch_size,name='g_bn11')
-	dec4 = g_bn11(deconv2d(dec3,[self.batch_size,32,32,self.df_dim*8],name='g_dec4'),train=False)#output 32x32
+	dec4 = tf.image.resize_nearest_neighbor(dec3,[32,32])
+	dec4 = tf.nn.dropout(g_bn11(conv2d(dec4,self.df_dim*8,k_h=4,k_w=4,name='g_dec4'),train=False),keep_prob) #output 32x32
 	dec4 = tf.nn.relu(tf.concat(3,[dec4,enc3]))
 	g_bn12 = batch_norm(self.batch_size,name='g_bn12')
-	dec5 = g_bn12(deconv2d(dec4,[self.batch_size,64,64,self.df_dim*4],name='g_dec5'),train=False)#output 64x64
+	dec5 = tf.image.resize_nearest_neighbor(dec4,[64,64])
+	dec5 = tf.nn.dropout(g_bn12(conv2d(dec5,self.df_dim*4,k_h=4,k_w=4,name='g_dec5'),train=False),keep_prob) #output 64x64
 	dec5 = tf.nn.relu(tf.concat(3,[dec5,enc2]))
 	g_bn13 = batch_norm(self.batch_size,name='g_bn13')
-	dec6 = g_bn13(deconv2d(dec5,[self.batch_size,128,128,self.df_dim*2],name='g_dec6'),train=False)#output 128x128
+	dec6 = tf.image.resize_nearest_neighbor(dec5,[128,128])
+	dec6 = tf.nn.dropout(g_bn13(conv2d(dec6,self.df_dim*2,k_h=4,k_w=4,name='g_dec6'),train=False),keep_prob) #output 128x128
 	dec6 = tf.nn.relu(tf.concat(3,[dec6,enc1]))
-	dec7 = deconv2d(dec6,[self.batch_size,256,256,3],name='g_dec7')#output 256x256
+	dec7 = tf.image.resize_nearest_neighbor(dec6,[256,256])
+	dec7 = conv2d(dec7,3,k_h=4,k_w=4,name='g_dec7') #output 256x256
 
 	return tf.nn.tanh(dec7)
